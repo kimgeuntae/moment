e9c35aab5027f19f126d418c05db1d00016cc89a
kimgeuntae <kok617@naver.com>
2019-11-15 14:41:23 +0900

    Add save with JSON [fn_saveToDos]

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoFrom = document.querySelector(".js-toDoFrom"),
 
 const TODOS_LS = "toDos";
 
-function paintToDo(text) {  // Create list in toDoList
+const toDos = [];
+
+function saveToDos() {  // Save todos with JSON in localstorage.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {  // Create list in toDoList.
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X"
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "X"
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -24,8 +38,10 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        console.log(parsedToDos);
     }
 }
 
