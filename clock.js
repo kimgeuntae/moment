fbfe0603cbb25470aaf7a4f45e34701bff2a424a
kimgeuntae <kok617@naver.com>
2019-11-18 16:19:33 +0900

    Add clock [fn_addZero]

--- clock.js
+++ clock.js
@@ -1,14 +1,23 @@
 const clockContainer = document.querySelector(".js-clock"),
     clockTitle = clockContainer.querySelector("h1");
 
+function addZero(time) {
+    return `${time < 10 ? `0${time}` : time}`
+}
+      
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const secodnds = date.getSeconds();
+    clockTitle.innerText = 
+    `${addZero(hours)}:${addZero(minutes)}:${addZero(secodnds)}`;
+
+    /*
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes
     }:${secodnds < 10 ? `0${secodnds}` : secodnds}`;
+    */
 }
 
 function init() {
